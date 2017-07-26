7fcd5388d3af509ef2adcbef5b524ce03ead4c2d
Jason Allen <jasonallenphotography@gmail.com>
2017-07-26 06:57:29 -0400

    Refactor dynamic JQuery view elements on users#show page

--- public/js/application.js
+++ public/js/application.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     });
   });
 
-  $('#new-location-form').on('submit',function(e){
+  $('#new-location-form').on('submit', 'form', function(e){
     e.preventDefault();
     var $target = $(e.target);
     $('#new-location-form').hide(500);
@@ -23,17 +23,27 @@ $(document).ready(function() {
       method: $target.attr('method'),
       data: $target.serialize()
     }).done(function(response){
-      $('.locations').prepend(response);
+      $('#new-location').show(500);
+      $('.locations').append(response).show(500);
       $('#new-location-form').hide(500);
       $('#new-location-form')[0].reset();
-      $('#new-location').show(500);
     });
   });
 
   $('.locations').on('click', 'a.edit-delete', function(e){
     e.preventDefault();
+    $(".edit-delete-span").hide(500);
+    $(".query-button").hide(500);
     var $target = $(e.target);
-    $target.closest('.location').find('.edit-delete-form-div').show(500)
+    $target.closest('.location').find('.edit-delete-form-div').show(500);
+  });
+
+  $('.locations').on('click', 'a.cancel', function(e){
+    e.preventDefault();
+    $(".edit-delete-span").show(500);
+    $(".query-button").show(500);
+    var $target = $(e.target);
+    $target.closest('.location').find('.edit-delete-form-div').hide(500);
   });
 
   $('.locations').on('submit', '.query-button', function(e){
